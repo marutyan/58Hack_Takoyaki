bb045326bf92825a9e0fefa9b722be694aa34c2c
marutyan <132348090+marutyan@users.noreply.github.com>
2024-09-21 03:35:25 +0900

    Implementing most of the screen transitions

--- my-app/src/app/UserTypeSelection/page.jsx
+++ my-app/src/app/UserTypeSelection/page.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 import ProgressIndicator from './ProgressIndicator';
 import OptionButton from './OptionButton';
 import styles from './styles';
+import {useRouter} from 'next/navigation';
 
 const UserTypeSelection = () => {
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    router.push('/AccountCreation');
+  }
+
   return (
     <>
       <main className="user-type-selection">
@@ -19,7 +27,7 @@ const UserTypeSelection = () => {
           <OptionButton id="studentOption" label="生徒" />
           <label htmlFor="teacherOption" className="visually-hidden">教員</label>
           <OptionButton id="teacherOption" label="教員" />
-          <button className="next-button" type="submit">次へ</button>
+          <button className="next-button" type="submit" onClick={handleSubmit}>次へ</button>
         </form>
       </main>
       <style jsx global>{styles}</style>
