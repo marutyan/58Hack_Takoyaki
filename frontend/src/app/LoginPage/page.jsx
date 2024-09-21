3ad9c6b4f05913a1537df6a3372e415ef9708305
marutyan <132348090+marutyan@users.noreply.github.com>
2024-09-21 23:28:31 +0900

    Railsの魔術師とやったところ

--- frontend/src/app/LoginPage/page.jsx
+++ frontend/src/app/LoginPage/page.jsx
@@ -16,16 +16,32 @@ function LoginPage() {
     router.push("/UserTypeSelection");
   }
 
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    //http://localhost:8000/loginにEmailとpasswordを送信
+    fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: "marutyan310@gmail.com",
+        password: "password"
+      })})
+
+  }
+  
+
   return (
     <main className="login-page">
       <h1 className="login-title">ログイン</h1>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleLoginClick}>
         {inputFields.map((field, index) => (
           <InputField key={index} label={field.label} type={field.type} />
         ))}
         <a href="#" className="forgot-password">パスワードを忘れた方はこちら</a>
         <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/cc77a74764ff26f241d9a035ceaad0ce5229f6bdc02975cfe2e76e9ab8960491?placeholderIfAbsent=true&apiKey=c5b43863a5b74dfaac997754dc46d7dc" alt="" className="decorative-image" />
-        <Button text="ログイン" />
+        <Button text="ログイン" onClick={handleLoginClick} />
         <hr className="divider" />
         <a href="#"
         className="create-account"
