9e037e47ecae4ea666bc132ec217d23115b19058
marutyan <132348090+marutyan@users.noreply.github.com>
2024-09-22 05:34:16 +0900

    サインアップの途中経過

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000' # Reactのポート番号に合わせて変更
     resource '*', methods: :any, headers: :any
   end
 end
--- frontend/src/app/AccountCreation/AccountCreationForm.jsx
+++ frontend/src/app/AccountCreation/AccountCreationForm.jsx
@@ -1,27 +1,88 @@
 "use client";
 import React from 'react';
-import FormField from './FormField';
 import {useRouter} from 'next/navigation';
+import InputField from '../LoginPage/InputField';
+import { useForm } from "react-hook-form";
+import { useCallback } from "react";
+import Select from './Select';
 
 const AccountCreationForm = () => {
   const router = useRouter();
-  const fields = [
-    { label: 'メールアドレス', id: 'email', type: 'email' },
-    { label: 'パスワード', id: 'password', type: 'password' },
-    { label: 'パスワード(確認用)', id: 'password-confirm', type: 'password' },
-    { label: 'アカウント名', id: 'account-name' },
-  ];
+  const { handleSubmit , register} = useForm();
+  
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    router.push('/PersonalInfoRegistration');
-  }
+  const handleCreationClick = useCallback(async (data) => {
+    try {
+      const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log(JSON.stringify(data));
+
+      if (!response.ok) {
+        console.error("responseがダメだったみたい", response);
+      }else{
+        router.push("/PersonalInfoRegistration");
+      }
+      throw new Error("アカウント作成に失敗しました");
+    } catch (error) {
+      console.log("catchに入ったよ", error);
+    }
+  });
+
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      {fields.map((field) => (
-        <FormField key={field.id} {...field} />
-      ))}
+    <form className="form-container" onSubmit={handleSubmit(handleCreationClick)}>
+      <InputField
+        label="メールアドレス"
+        type="email"
+        id="emailInput"
+        {...register("email", { required: "emailを入力してください" })}
+      />
+      <InputField
+        label="パスワード"
+        type="password"
+        id="passwordInput"
+        {...register("password", { required: "パスワードを入力してください" })}
+      />
+      <InputField
+        label="パスワード(確認用)"
+        type="password"
+        id="Confirmationpassword"
+      />
+      <InputField
+        label="アカウント名"
+        type="text"
+        id="name"
+        {...register("name", { required: "アカウント名を入力してください" })}
+      />
+      
+      <Select
+      label="学年"
+      id="grade"
+      {...register("age", { required: "学年を入力してください" })}
+      >
+        <option value="高校１年生">高校１年生</option>
+        <option value="高校２年生">高校２年生</option>
+        <option value="高校３年生">高校３年生</option>
+      </Select>
+      
+      <Select
+      label="性別"
+      id="gender"
+      {...register("gender", { required: "性別を入力してください" })}
+      >
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </Select>
+
       <button type="submit" className="submit-button">次へ</button>
     </form>
   );
