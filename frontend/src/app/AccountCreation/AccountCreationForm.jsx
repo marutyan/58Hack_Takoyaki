e693af3f93eba243355551a6d1e477a6bae70433
Shi-28 <2312110372b@kindai.ac.jp>
2024-09-22 03:30:44 +0900

    sign-up途中2

--- frontend/src/app/AccountCreation/AccountCreationForm.jsx
+++ frontend/src/app/AccountCreation/AccountCreationForm.jsx
@@ -1,27 +1,60 @@
 "use client";
 import React from 'react';
-import FormField from './FormField';
 import {useRouter} from 'next/navigation';
+import InputField from '../LoginPage/InputField';
+import { useForm } from "react-hook-form";
+import { useCallback } from "react";
 
 const AccountCreationForm = () => {
   const router = useRouter();
-  const fields = [
-    { label: 'メールアドレス', id: 'email', type: 'email' },
-    { label: 'パスワード', id: 'password', type: 'password' },
-    { label: 'パスワード(確認用)', id: 'password-confirm', type: 'password' },
-    { label: 'アカウント名', id: 'account-name' },
-  ];
+  const { handleSubmit , register} = useForm();
+  
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    router.push('/PersonalInfoRegistration');
-  }
+  const handleCreationClick = useCallback(async (data) => {
+    try {
+      const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        // mode: "cors",
+        // credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        console.error("responseがダメだったみたい", response);
+      }else{
+        router.push("/PersonalInfoRegistration");
+      }
+      throw new Error("アカウント作成に失敗しました");
+    } catch (error) {
+      console.log("catchに入ったよ", error);
+    }
+  });
+
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      {fields.map((field) => (
-        <FormField key={field.id} {...field} />
-      ))}
+    <form className="form-container" onSubmit={handleSubmit(handleCreationClick)}>
+      <InputField
+        label="メールアドレス"
+        type="email"
+        {...register("email", { required: "emailを入力してください" })}
+      />
+      <InputField
+        label="パスワード"
+        type="password"
+        {...register("password", { required: "パスワードを入力してください" })}
+      />
+      <InputField
+        label="パスワード(確認用)"
+        type="password"
+      />
+      <InputField
+        label="アカウント名"
+        type="text"
+        {...register("account-name", { required: "アカウント名を入力してください" })}
+      />
       <button type="submit" className="submit-button">次へ</button>
     </form>
   );
--- frontend/src/app/AccountCreation/FormField.jsx
+++ frontend/src/app/AccountCreation/FormField.jsx
@@ -1,11 +1,53 @@
 "use client";
-import React from 'react';
+import React, { forwardRef } from "react";
 
-const FormField = ({ label, id, type = 'text' }) => (
-  <>
-    <label htmlFor={id} className="form-label">{label}</label>
-    <input type={type} id={id} className="form-input" aria-label={label} />
-  </>
-);
+
+const FormField = forwardRef(({ label, type, ...props }, ref) => {
+  // <>
+  //   <label htmlFor={id} className="form-label">{label}</label>
+  //   <input type={type} id={id} className="form-input" aria-label={label} />
+  // </>
+  return (
+    <div className="input-group">
+      <label htmlFor={`${type}Input`} className="input-label">{label}</label>
+      <input
+        type={type}
+        id={`${type}Input`}
+        className="input-field"
+        placeholder={label}
+        aria-label={label}
+        ref={ref}
+        {...props}
+      />
+      <style jsx>{`
+        .input-group {
+          display: flex;
+          flex-direction: column;
+          margin-bottom: 25px;
+        }
+        .input-label {
+          font-family: Inter, sans-serif;
+          line-height: 1;
+          font-weight: 700;
+          margin-bottom: 12px;
+        }
+        .input-field {
+          border-radius: 3px;
+          background-color: #f6f3f3;
+          min-height: 30px;
+          padding: 5px 10px;
+          border: none;
+          font-weight: 400;
+          color:#000;
+        }
+      `}</style>
+    </div>
+  );
+});
+
+FormField.displayName = "FormField";
+
+
+
 
 export default FormField;
